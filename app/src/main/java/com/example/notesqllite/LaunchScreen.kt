e2d1dfee89ae755d15faac91854960926311baae
Malindu Naveen <naveenmalindu5@gmail.com>
2024-05-13 15:19:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notesqllite/LaunchScreen.kt
@@ -0,0 +1,19 @@
+package com.example.notesqllite
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class LaunchScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+                              val intent = Intent(this@LaunchScreen,MainActivity::class.java)
+                              startActivity(intent)
+        },3000)
+    }
+}
